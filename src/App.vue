fa81d530d968f7c10454c2b80ade9d24dca646f9
joaosantoz <jvsantosdev@gmail.com>
2023-04-09 01:42:34 -0300

    feat: initial scss config and variables

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
  <h1>Vue</h1>
 </template>
+
+<style lang="scss">
+h1 {
+  color: $color-primary;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import './assets/styles/main.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
