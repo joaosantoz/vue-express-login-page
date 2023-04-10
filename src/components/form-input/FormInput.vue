6488dd8aa7163b5360e0ce48e933618b462988a5
joaosantoz <jvsantosdev@gmail.com>
2023-04-10 20:20:10 -0300

    feat: creating form input component

---
+++ src/components/form-input/FormInput.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue';
+import { requiredValidator } from "@/validation/requiredValidator";
+
+const props = defineProps(['fieldOptions']);
+const fieldOptions = computed(() => props.fieldOptions);
+
+
+const validateInputContent = () => {
+  fieldOptions.value.isValid = requiredValidator(fieldOptions.value.content);
+}
+
+</script>
+
+<template>
+  <label>{{ fieldOptions.title }}</label>
+  <input type="text" v-model="fieldOptions.content" @input="validateInputContent()">
+</template>
