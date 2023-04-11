9adf63f66f771611e273ca56437ac85f0a708f0e
joaosantoz <jvsantosdev@gmail.com>
2023-04-11 09:03:37 -0300

    style: reformatting after eslint changes

--- src/pages/registration-form/RegistrationForm.vue
+++ src/pages/registration-form/RegistrationForm.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { useStepper } from '@vueuse/core'
+import { useStepper } from '@vueuse/core';
+import { computed, ref, watch } from 'vue';
 import { useRegistrationFormStore } from '@/stores/registration-form/registrationForm';
-import { computed, ref, watch } from "vue";
-import FormInput from "@/components/form-input/FormInput.vue";
+import FormInput from '@/components/form-input/FormInput.vue';
 
 const store = useRegistrationFormStore();
 
@@ -15,11 +15,11 @@ const stepper = useStepper({
   },
   password: {
     title: 'Senha de acesso',
-  },	
+  },
   review: {
-    title: 'Revise suas informações'
-  }
-})
+    title: 'Revise suas informações',
+  },
+});
 
 const form = computed(() => store.form);
 
@@ -27,39 +27,97 @@ const personType = ref(store.personType);
 
 const changePersonType = () => {
   store.setPersonType(personType.value);
-}
+};
 
-const currentFormIsValid = computed(() => {
-  return form.value.map((field) => field.isValid).every((itemValid) => itemValid);
-})
+const formIsValid = computed(() => form.value.map((item) => item.isValid).every((valid) => valid));
 
 watch(stepper.index, () => {
   store.setCurrentStep(stepper.index.value);
-})
+});
 
 </script>
 
 <template>
-  <h1>{{ stepper.index }}</h1>
+  <form class="registration-form">
+    <div class="form-information">
+      <p class="form-step">
+        Etapa <span class="current-step">{{ stepper.index.value + 1 }}</span>
+        de {{ stepper.stepNames.value.length }}
+      </p>
+      <h1 class="form-title">
+        {{ stepper.current.value.title }}
+      </h1>
+    </div>
 
-  <h1>{{ store.personType }}</h1>
+    <div>
+      <FormInput
+        v-for="(field, index) in form"
+        :key="index"
+        :field-options="field"
+      />
+    </div>
 
-  <div>
-    <FormInput v-for="(field, index) in form" :key="index" :fieldOptions="field"></FormInput>
-  </div>
+    <div v-if="stepper.isFirst.value">
+      <input
+        id="pf"
+        type="radio"
+        value="pf"
+        name="person"
+        v-model="personType"
+        @change="changePersonType()"
+      >
+      <label for="pf">Pessoa Física</label>
 
-  <div v-if="stepper.isFirst.value">
-    <input id="pf" type="radio" value="pf" name="person" v-model="personType" @change="changePersonType()">
-    <label for="pf">Pessoa Física</label>
+      <input
+        id="pj"
+        type="radio"
+        value="pj"
+        name="person"
+        v-model="personType"
+        @change="changePersonType()"
+      >
+      <label for="pj">Pessoa Jurídica</label>
+    </div>
 
-    <input id="pj" type="radio" value="pj" name="person" v-model="personType" @change="changePersonType()">
-    <label for="pj">Pessoa Jurídica</label>
-  </div>
-
-  <p>{{ JSON.stringify(form, null, 4) }}</p>
-
-  <p>{{ currentFormIsValid }}</p>
-
-  <button @click="stepper.goToPrevious()" v-if="!stepper.isFirst.value">prev</button>
-  <button @click="stepper.goToNext()" v-if="!stepper.isLast.value" :disabled="!currentFormIsValid">next</button>
+    <button
+      @click="stepper.goToPrevious()"
+      v-if="!stepper.isFirst.value"
+    >
+      Voltar
+    </button>
+    <div>
+      <button
+        @click="stepper.goToNext()"
+        v-if="!stepper.isLast.value"
+        :disabled="!formIsValid"
+      >
+        Continuar
+      </button>
+      <button
+        v-if="stepper.isLast.value"
+        :disabled="!formIsValid"
+      >
+        Cadastrar
+      </button>
+    </div>
+  </form>
 </template>
+
+<style lang="scss">
+.registration-form {
+  border: 1px solid tomato;
+  max-width: 30%;
+  margin: 0 auto;
+
+  .form-information {
+    .form-title {
+      font-weight: bold;
+    }
+    .form-step {
+      .current-step {
+        color: $color-primary;
+      }
+    }
+  }
+}
+</style>
