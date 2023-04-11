2741e8147883a3839c148eff5c2478625cbc2e4f
joaosantoz <jvsantosdev@gmail.com>
2023-04-11 14:01:23 -0300

    feat: creating the visual of form input

--- src/components/form-input/FormInput.vue
+++ src/components/form-input/FormInput.vue
@@ -5,7 +5,6 @@ import { requiredValidator } from '@/validation/validators/requiredValidator';
 const props = defineProps({
   fieldOptions: {
     type: Object,
-    default: null,
     required: true,
   },
 });
@@ -18,10 +17,35 @@ const validateInputContent = () => {
 </script>
 
 <template>
-  <label>{{ fieldOptions.title }}</label>
-  <input
-    type="text"
-    v-model="fieldOptions.content"
-    @input="validateInputContent()"
-  >
+  <div class="form-input-wrapper">
+    <label class="input-title">{{ fieldOptions.title }}</label>
+    <input
+      class="form-input"
+      type="text"
+      v-model="fieldOptions.content"
+      @input="validateInputContent()"
+    >
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.form-input-wrapper {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 1em;
+
+  .input-title {
+    width: 100%;
+  }
+
+  .form-input {
+    margin-top: 0.5em;
+    width: 100%;
+    height: 2.5em;
+    border: 1px solid $color-grey;
+    border-radius: 0.5em;
+    padding: 0.25em;
+  }
+}
+</style>
