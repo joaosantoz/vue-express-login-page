a182f6d9281c7d689e13428868c4233446347a9d
joaosantoz <jvsantosdev@gmail.com>
2023-04-12 13:32:15 -0300

    test: creating tests for requiredValidator

---
+++ src/validation/required/requiredValidator.spec.js
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest';
+import { requiredValidator } from "@/validation";
+
+describe('Required Validator', () => {
+  const sut = requiredValidator;
+
+  it('should return error message when empty string is given', () => {
+    const inputValue = '';
+
+    expect(sut(inputValue)).toBe('Este campo é obrigatório.');
+  });
+
+  it('should return error message when null is given', () => {
+    const inputValue = null;
+
+    expect(sut(inputValue)).toBe('Este campo é obrigatório.');
+  });
+
+  it('should return error message when only spaces are given', () => {
+    const inputValue = '   ';
+
+    expect(sut(inputValue)).toBe('Este campo é obrigatório.');
+  });
+
+  it('should return false when a valid value is given', () => {
+    const inputValue = 'anyValidValue';
+
+    expect(sut(inputValue)).toBeFalsy();
+  });
+});
