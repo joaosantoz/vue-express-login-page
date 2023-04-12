b4c427c6a6ae2b293a52df4c1e27f98fdb34062b
joaosantoz <jvsantosdev@gmail.com>
2023-04-12 13:30:05 -0300

    feat: creating an emailValidator

---
+++ src/validation/email/emailValidator.js
@@ -0,0 +1,11 @@
+export const emailValidator = (content) => {
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if (!content) return false;
+
+  if (!regex.test(content)) {
+    return 'Digite um e-mail válido.';
+  }
+
+  return false;
+};
