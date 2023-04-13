f0357772717e09a569e2d46823866e25018edbfe
joaosantoz <jvsantosdev@gmail.com>
2023-04-13 14:03:03 -0300

    feat: importing info from dotenv on server

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+import * as path from 'path';
 import app from './app.js';
 
-const PORT = 3000;
+dotenv.config({
+  path: path.resolve('../../', '.env'),
+});
+
+const PORT = process.env.VITE_API_PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
